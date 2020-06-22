0d5f9aa4e58cc516ee4d1faf85fc3b9497ffba52
koyeary <katyeary@gmail.com>
2020-06-22 15:10:19 -0700

    Added seed document

--- seed.sql
+++ seed.sql
@@ -1,1 +1,22 @@
-seed
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  flavor VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO department (name)
+VALUES ("administration");
+
+INSERT INTO role (title, salary)
+VALUES ("manager", 79000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Tara", "Mintle");
