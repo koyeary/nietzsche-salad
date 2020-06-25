6f3045d225106f9b747bf2b6ec7122370160f601
koyeary <katyeary@gmail.com>
2020-06-24 17:46:16 -0700

    complete seed, remove unnecessary comments

--- seed.sql
+++ seed.sql
@@ -10,18 +10,18 @@ INSERT into department (name, department_id) VALUES ("Traders", 3);
 INSERT into department (name, department_id) VALUES ("Marketing", 4);
 INSERT into department (name, department_id) VALUES ("Administration", 5);
 
-INSERT into role (title, salary, department_id) VALUES ("Research Analyst", 111560, 1); /* portfolio management */
-INSERT into role (title, salary, department_id) VALUES ("Director of Research", 146883, 1); /* portfolio management */
-INSERT into role (title, salary, department_id) VALUES ("Quant", 121664, 1); /* portfolio management */
-INSERT into role (title, salary, department_id) VALUES ("Senior Portfolio Manager", 526000, 1); /* portfolio management */
-INSERT into role (title, salary, department_id) VALUES ("Junior Analyst", 102031, 2); /* Risk Management */
-INSERT into role (title, salary, department_id) VALUES ("Risk and Reporting Manager", 123646, 2); /* Risk Management */
-INSERT into role (title, salary, department_id) VALUES ("Bonds & Equities", 74582, 3); /* traders */
-INSERT into role (title, salary, department_id) VALUES ("Options", 83180, 3); /* traders */
-INSERT into role (title, salary, department_id) VALUES ("Project Manager", 89000, 4); /* Marketing */
-INSERT into role (title, salary, department_id) VALUES ("Copywriter", 56000, 4); /* Marketing */
-INSERT into role (title, salary, department_id) VALUES ("Office Manager", 60000, 5); /* Administration */
-INSERT into role (title, salary, department_id) VALUES ("Receptionist", 37525, 5); /* Administration */
+INSERT into role (title, salary, department_id) VALUES ("Research Analyst", 111560, 1);
+INSERT into role (title, salary, department_id) VALUES ("Director of Research", 146883, 1); 
+INSERT into role (title, salary, department_id) VALUES ("Quant", 121664, 1); 
+INSERT into role (title, salary, department_id) VALUES ("Senior Portfolio Manager", 526000, 1); 
+INSERT into role (title, salary, department_id) VALUES ("Junior Analyst", 102031, 2); 
+INSERT into role (title, salary, department_id) VALUES ("Risk and Reporting Manager", 123646, 2); 
+INSERT into role (title, salary, department_id) VALUES ("Bonds & Equities", 74582, 3); 
+INSERT into role (title, salary, department_id) VALUES ("Options", 83180, 3); 
+INSERT into role (title, salary, department_id) VALUES ("Project Manager", 89000, 4); 
+INSERT into role (title, salary, department_id) VALUES ("Copywriter", 56000, 4); 
+INSERT into role (title, salary, department_id) VALUES ("Office Manager", 60000, 5); 
+INSERT into role (title, salary, department_id) VALUES ("Receptionist", 37525, 5); 
 
 INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Richard", "Medley", 1.1, null);
 INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Ross", "Cameron", 1.2, 10);
